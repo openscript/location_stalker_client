52c0ccca314da21d8c1b1e25c8578e4d54c411c6
Robin <rdex@external.(none)>
2014-05-08 16:47:53 +0200

    send position ability added

--- scripts/app.js
+++ scripts/app.js
@@ -5,8 +5,6 @@ var opts = {
 	maximumAge: 0
 };
 
-var data = {};
-
 // events
 $(document).ready(function() {
 	addLog('Location Stalker Client initialized');
@@ -24,6 +22,28 @@ $('#changin-positions .initialize').click(function() {
 	setLoc('#changin-positions');
 });
 
+$('#single-position form').submit(function() {
+	var data = {};
+	data['latitude'] = $(this).children('input[name="latitude"]').val();
+	data['longitude'] = $(this).children('input[name="longitude"]').val();
+	data['altitude'] = $(this).children('input[name="altitude"]').val();
+	data['accuracy'] = $(this).children('input[name="accuracy"]').val();
+	sendData(data);
+
+	return false;
+});
+
+$('#changin-positions form').submit(function() {
+	if($(this).children('.stop').hasClass('hide')){
+		$(this).children('.start').addClass('hide');
+		$(this).children('.stop').removeClass('hide');
+	} else {
+		$(this).children('.start').removeClass('hide');
+		$(this).children('.stop').addClass('hide');
+	}
+	return false;
+});
+
 // functions
 var addLog = function(message) {
 	$('#log .messages').prepend($('<li>').text('[' + $.formatDateTime('dd.mm.y hh:ii:ss', new Date()) + '] ~ ' + message));
@@ -37,7 +57,7 @@ var setLoc = function(target) {
 			$(target + ' input[name="longitude"]').val(res.coords.longitude);
 			$(target + ' input[name="altitude"]').val(res.coords.altitude);
 			$(target + ' input[name="accuracy"]').val(res.coords.accuracy);
-			addLog('Geolocation set');
+			addLog('Geolocation set: ' + JSON.stringify(res.coords));
 		}, function(err) {
 			addLog('Error occured during geolocation retrival..');
 		}, opts);
@@ -45,5 +65,13 @@ var setLoc = function(target) {
 };
 
 var sendData = function(data) {
-	
+	$.ajax({
+		type: 'POST',
+		url: $('#target').val(),
+		data: data,
+		success: function() {
+			addLog('Data sent: ' + JSON.stringify(data))
+		},
+		dataType: 'json'
+	});
 }
